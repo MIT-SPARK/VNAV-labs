ed9d62fd81b6e0a704a2c72ee8d23a6a64a7fef0
Aaron Ray <aaronray@mit.edu>
2024-08-25 13:55:08 -0400

    Update lab2 stencil code

--- lab2/two_drones_pkg/src/frames_publisher_node.cpp
+++ lab2/two_drones_pkg/src/frames_publisher_node.cpp
@@ -1,83 +1,90 @@
-#include <ros/ros.h>
-#include <tf2_ros/transform_broadcaster.h>
 #include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/transform_broadcaster.h>
 
+#include <geometry_msgs/msg/transform_stamped.hpp>
 #include <iostream>
+#include <memory>
+#include <rclcpp/create_timer.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp/timer.hpp>
 
-class FramesPublisherNode {
- private:
-  ros::NodeHandle nh;
-  ros::Time startup_time;
+class FramesPublisherNode : public rclcpp::Node {
+  rclcpp::Time startup_time;
 
-  ros::Timer heartbeat;
-  // *** FILL IN *** instantiate a transform broadcaster...
+  rclcpp::TimerBase::SharedPtr heartbeat;
+
+  // TODO: Declare a std::unique_ptr<tf2_ros::TransformBroadcaster>
+  // ...
 
  public:
-  FramesPublisherNode() {
+  FramesPublisherNode() : Node("frames_publisher_node") {
     // NOTE: This method is run once, when the node is launched.
-    startup_time = ros::Time::now();
-    heartbeat =
-        nh.createTimer(ros::Duration(0.02), &FramesPublisherNode::onPublish, this);
-    heartbeat.start();
+
+    // TODO: Instantiate the Transform Broadcaster
+    // ....
+
+    startup_time = now();
+    heartbeat = create_timer(this,
+                             get_clock(),
+                             rclcpp::Duration::from_seconds(0.02),
+                             std::bind(&FramesPublisherNode::onPublish, this));
+    heartbeat->reset();
   }
 
-  void onPublish(const ros::TimerEvent&) {
-    // NOTE: This method is called at 50Hz, due to the timer created on line 16.
+  void onPublish() {
+    // NOTE: This method is called at 50Hz, due to the timer created on line 25.
 
-    // 1. Compute time elapsed in seconds since the node has been started
-    //    i.e. the time elapsed since startup_time (defined on line 8)
+    //
+    // 1. TODO: Compute time elapsed in seconds since the node has been started
+    //    i.e. the time elapsed since startup_time (defined on line 13)
     //   HINTS:
-    //   - check out the ros::Time API at
-    //     http://wiki.ros.org/roscpp/Overview/Time#Time_and_Duration
-    //   - use the - (subtraction) operator between ros::Time::now() and startup_time
+    //   - Get the current time with this->now()
+    //   - use the - operator between the current time and startup_time
     //   - convert the resulting Duration to seconds, store result into a double
 
-    // double time = *** FILL IN ***;
+    // ...
 
-    // Here we declare two geometry_msgs::TransformStamped objects, which need to be
-    // populated
-    geometry_msgs::TransformStamped AV1World;
-    geometry_msgs::TransformStamped AV2World;
-    // NOTE: fields in a ros message default to zero, so we set an identity transform by
-    //       setting just the w component of the rotation
-    AV1World.transform.rotation.w = 1.0;
-    AV2World.transform.rotation.w = 1.0;
+    // Here we declare two geometry_msgs::msg::TransformStamped objects, which
+    // need to be populated
+    geometry_msgs::msg::TransformStamped world_T_av1;
+    geometry_msgs::msg::TransformStamped world_T_av2;
 
-    // 2. Populate the two transforms for the AVs, using the variable "time"
+    // 2. TODO: Populate the two transforms for the AVs, using the variable "time"
     //    computed above. Specifically:
-    //     - AV1World should have origin in [cos(time), sin(time), 0.0] and
+    //     - world_T_av1 should have origin in [cos(time), sin(time), 0.0] and
     //       rotation such that:
     //        i) its y axis stays tangent to the trajectory and
     //       ii) the z vector stays parallel to that of the world frame
-    //     - AV1World should have frame_id "world" and child_frame_id "av1"
     //
-    //     - AV2World shoud have origin in [sin(time), 0.0, cos(2*time)], the
+    //     - world_T_av2 shoud have origin in [sin(time), 0.0, cos(2*time)], the
     //       rotation is irrelevant to our purpose.
-    //     - AV2World should have frame_id "world" and child_frame_id "av2"
-    //    NOTE: AV1World's orientation is crucial for the rest fo the assignment,
+    //    NOTE: world_T_av1's orientation is crucial for the rest fo the
+    //    assignment,
     //          make sure you get it right
     //
     //    HINTS:
-    //    1. check out the ROS tf2 Tutorials: http://wiki.ros.org/tf2/Tutorials,
-    //      https://wiki.ros.org/tf2/Tutorials/Adding%20a%20frame%20%28C%2B%2B%29#The_Code
-    //    2. consider the setRPY method on a tf2::Quaternion for AV1
-    //    3. the frame names are crucial for the rest of the assignment,
-    //       make sure they are as specified, "av1", "av2" and "world"
+    //    - check out the ROS tf2 Tutorials:
+    //    https://docs.ros.org/en/humble/Tutorials/Intermediate/Tf2/Tf2-Main.html
+    //    - consider the setRPY method on a tf2::Quaternion for world_T_av1
 
-    // *** FILL IN ***
+    // ...
 
-    // 3. Publish the transforms using a tf2_ros::TransformBroadcaster
+    // 3. TODO: Publish the transforms, namely:
+    //     - world_T_av1 with frame_id "world", child_frame_id "av1"
+    //     - world_T_av2 with frame_id "world", child_frame_id "av2"
     //    HINTS:
-    //         1. you need to define a tf2_ros::TransformBroadcaster as a member of the
-    //            node class (line 11) and use its sendTrasform method below
+    //         1. you need to define a
+    //         std::unique_ptr<tf2_ros::TransformBroadcaster> as a member of the
+    //            node class (line 18) and use its sendTransform method below
+    //         2. the frame names are crucial for the rest of the assignment,
+    //            make sure they are as specified, "av1", "av2" and "world"
 
-    // *** FILL IN ***
+    // ...
   }
 };
 
 int main(int argc, char** argv) {
-  ros::init(argc, argv, "frames_publisher_node");
-  FramesPublisherNode node;
-  ros::spin();
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<FramesPublisherNode>());
   return 0;
 }
--- lab2/two_drones_pkg/src/plots_publisher_node.cpp
+++ lab2/two_drones_pkg/src/plots_publisher_node.cpp
@@ -1,94 +1,113 @@
-#include <geometry_msgs/Point.h>
-#include <ros/ros.h>
-#include <tf2_ros/transform_listener.h>
-#include <visualization_msgs/MarkerArray.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2/exceptions.h>
+#include <tf2/time.h>
+#include <tf2_ros/buffer_interface.h>
+#include <tf2_ros/transform_broadcaster.h>
 
+#include <geometry_msgs/msg/transform_stamped.hpp>
 #include <iostream>
 #include <list>
+#include <memory>
+#include <rclcpp/create_timer.hpp>
+#include <rclcpp/duration.hpp>
+#include <rclcpp/publisher.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp/timer.hpp>
+#include <visualization_msgs/msg/marker.hpp>
+#include <visualization_msgs/msg/marker_array.hpp>
 
-class PlotsPublisherNode {
-  ros::Time startup_time;
-  ros::Timer heartbeat;
-  ros::NodeHandle nh;
-  ros::Publisher markers_pub;
-  tf2_ros::TransformListener tf_listener;
-  tf2_ros::Buffer tf_buffer;
+#include "tf2_ros/buffer.h"
+#include "tf2_ros/transform_listener.h"
+
+class PlotsPublisherNode : public rclcpp::Node {
+  rclcpp::Time startup_time;
+  rclcpp::TimerBase::SharedPtr heartbeat;
+  rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr markers_pub;
+
+  std::shared_ptr<tf2_ros::TransformListener> tf_listener_{nullptr};
+  std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+
   int num_trails;
 
   class TrajTrail {
     PlotsPublisherNode* parent;
     static int id;
-    std::list<geometry_msgs::Point> poses;
+    std::list<geometry_msgs::msg::Point> poses;
     std::string ref_frame, dest_frame;
     size_t buffer_size;
 
     std::string ns;
-    float r, g, b, a;
 
-    visualization_msgs::Marker marker_out;
+    visualization_msgs::msg::Marker marker_out;
 
     void update() {
-      // NOTE: you need to populate this transform
-      geometry_msgs::TransformStamped transform;
+      geometry_msgs::msg::TransformStamped
+          transform;  // NOTE: you need to populate this transform
       try {
-        // ~~~~~~~~~~~~~~~~~~~~~~  BEGIN OF EDIT SECTION  ~~~~~~~~~~~~~~~~~~~~~~~~~
+        // ~~~~~~~~~~~~~~~~~~~~~~  BEGIN OF EDIT SECTION ~~~~~~~~~~~~~~~~~~~~~~~~~
 
         /* The transform object needs to be populated with the most recent
          * transform from ref_frame to dest_frame as provided by tf.
 
          * Relevant variables in this scope:
-         *   - ref_frame, the frame of reference relative to which the trajectory
+         *   - ref_frame, the frame of reference relative to which the
+         trajectory
          *                needs to be plotted (given)
          *   - dest_frame, the frame of reference of the object whose trajectory
          *                 needs to be plotted (given)
-         *   - parent->tf_buffer, a tf2_ros::Buffer object (given)
-         *   - transform, the geometry_msgs::TransformStamped object that needs to be populated
+         *   - parent->tf_listener, a tf2_ros::TransformListener object (given)
+         *   - transform, the transform object that needs to be populated
          *
          * HINT: use "lookupTransform", see
-         * https://wiki.ros.org/tf2/Tutorials/Writing%20a%20tf2%20listener%20%28C%2B%2B%29#TheCode
+         *
+         https://docs.ros.org/en/rolling/Tutorials/Intermediate/Tf2/Writing-A-Tf2-Listener-Cpp.html
+         *
+         * NOTE: only using "lookupTransform", you might see a lot of errors in
+         your terminal!
+         *       Unlike other topic types - which are typically received on a
+         callback basis -
+         *       \tf can be polled at any time and therefore "lookupTransform"
+         might fail
+         *       due to timing issues. To fix it, consider the method
+         "canTransform" with
+         *       a small wait time.
          */
 
-        // *** FILL IN ***
+        // TODO: fill in here
 
-        // ~~~~~~~~~~~~~~~~~~~~~~~~  END OF EDIT SECTION  ~~~~~~~~~~~~~~~~~~~~~~~~~
-        while (poses.size() >= buffer_size) {
-          poses.pop_front();
-        }
+        // ~~~~~~~~~~~~~~~~~~~~~~~~  END OF EDIT SECTION ~~~~~~~~~~~~~~~~~~~~~~~~~
+        while (poses.size() >= buffer_size) poses.pop_front();
 
-        geometry_msgs::Point tmp;
+        geometry_msgs::msg::Point tmp;
         tmp.x = transform.transform.translation.x;
         tmp.y = transform.transform.translation.y;
         tmp.z = transform.transform.translation.z;
         poses.push_back(tmp);
       } catch (const tf2::TransformException& ex) {
-        ROS_ERROR_STREAM("transform lookup failed: " << ex.what());
+        RCLCPP_ERROR_STREAM(parent->get_logger(),
+                            "transform lookup failed: " << ex.what());
       }
     }
 
    public:
-    TrajTrail() : parent(nullptr){};
+    TrajTrail() : parent(NULL){};
 
     TrajTrail(PlotsPublisherNode* parent_,
               const std::string& ref_frame_,
               const std::string& dest_frame_,
-              int buffer_size_ = 160)
+              int buffer = 160)
         : parent(parent_),
           ref_frame(ref_frame_),
           dest_frame(dest_frame_),
-          buffer_size(buffer_size_) {
-      if (buffer_size <= 0) {
-        ROS_ERROR_STREAM("invalid buffer size! defaulting to 10");
-        buffer_size = 10;
-      }
-
+          buffer_size(buffer) {
       marker_out.header.frame_id = ref_frame;
       marker_out.ns = "trails";
       marker_out.id = parent->num_trails++;
-      marker_out.type = visualization_msgs::Marker::LINE_STRIP;
-      marker_out.action = visualization_msgs::Marker::ADD;
+      marker_out.type = visualization_msgs::msg::Marker::LINE_STRIP;
+      marker_out.action = visualization_msgs::msg::Marker::ADD;
       marker_out.color.a = 0.8;
       marker_out.scale.x = 0.02;
-      marker_out.lifetime = ros::Duration(1.0);
+      marker_out.lifetime = rclcpp::Duration::from_seconds(1.0);
     }
 
     void setColor(float r_, float g_, float b_) {
@@ -99,91 +118,90 @@ class PlotsPublisherNode {
 
     void setNamespace(const std::string& ns_) { marker_out.ns = ns_; }
 
-    void setDashed() { marker_out.type = visualization_msgs::Marker::LINE_LIST; }
+    void setDashed() { marker_out.type = visualization_msgs::msg::Marker::LINE_LIST; }
 
-    visualization_msgs::Marker getMarker() {
+    visualization_msgs::msg::Marker getMarker() {
       update();
-      marker_out.header.stamp = ros::Time::now();
+      marker_out.header.stamp = parent->now();
       marker_out.points.clear();
       for (auto& p : poses) marker_out.points.push_back(p);
 
-      if (marker_out.type == visualization_msgs::Marker::LINE_LIST && poses.size() % 2)
+      if (marker_out.type == visualization_msgs::msg::Marker::LINE_LIST &&
+          poses.size() % 2)
         marker_out.points.resize(poses.size() - 1);
 
       return marker_out;
     }
   };
 
-  TrajTrail av1trail;
-  TrajTrail av2trail;
-  TrajTrail av2trail_rel;
+  TrajTrail av1trail, av2trail, av2trail_rel;
 
  public:
-  PlotsPublisherNode() : tf_listener(tf_buffer), num_trails(0) {
-    startup_time = ros::Time::now();
-    markers_pub = nh.advertise<visualization_msgs::MarkerArray>("visuals", 0);
-    heartbeat =
-        nh.createTimer(ros::Duration(0.02), &PlotsPublisherNode::onPublish, this);
-    heartbeat.start();
+  PlotsPublisherNode() : Node("plots_publisher_node"), num_trails(0) {
+    RCLCPP_INFO_STREAM(get_logger(), "Started plot node");
+
+    tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+    tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+
+    startup_time = now();
+    markers_pub = create_publisher<visualization_msgs::msg::MarkerArray>("visuals", 0);
+
+    RCLCPP_INFO_STREAM(get_logger(), "about to start heartbeat");
+    heartbeat = create_timer(this,
+                             get_clock(),
+                             rclcpp::Duration::from_seconds(0.02),
+                             std::bind(&PlotsPublisherNode::onPublish, this));
+    heartbeat->reset();
+    RCLCPP_INFO_STREAM(get_logger(), "started heartbeat");
+
     av1trail = TrajTrail(this, "world", "av1", 300);
-    av1trail.setColor(0.25, 0.52, 1.0);
+    av1trail.setColor(.25, .52, 1);
     av1trail.setNamespace("Trail av1-world");
     av2trail = TrajTrail(this, "world", "av2", 300);
-    av2trail.setColor(0.8, 0.4, 0.26);
+    av2trail.setColor(.8, .4, .26);
     av2trail.setNamespace("Trail av2-world");
     av2trail_rel = TrajTrail(this, "av1", "av2", 160);
     av2trail_rel.setDashed();
-    av2trail_rel.setColor(0.8, 0.4, 0.26);
+    av2trail_rel.setColor(.8, .4, .26);
     av2trail_rel.setNamespace("Trail av2-av1");
-
-    ROS_INFO_STREAM("Waiting for av1 and av2 transforms to be broadcast...");
-    while (ros::ok()) {
-      const bool av1_present = tf_buffer.canTransform("av1", "world", ros::Time(0));
-      const bool av2_present = tf_buffer.canTransform("av2", "world", ros::Time(0));
-      if (av1_present && av2_present) {
-        ROS_INFO_STREAM("Necessary frames are present, starting!");
-        break;
-      }
-    }
   }
 
-  void onPublish(const ros::TimerEvent&) {
-    visualization_msgs::MarkerArray visuals;
+  void onPublish() {
+    visualization_msgs::msg::MarkerArray visuals;
     visuals.markers.resize(2);
-    visualization_msgs::Marker& av1(visuals.markers[0]);
-    visualization_msgs::Marker& av2(visuals.markers[1]);
+    visualization_msgs::msg::Marker& av1(visuals.markers[0]);
+    visualization_msgs::msg::Marker& av2(visuals.markers[1]);
     av1.header.frame_id = "av1";
     av1.ns = "AVs";
     av1.id = 0;
-    av1.header.stamp = ros::Time();
-    av1.type = visualization_msgs::Marker::MESH_RESOURCE;
+    av1.header.stamp = now();
+    av1.type = visualization_msgs::msg::Marker::MESH_RESOURCE;
     av1.mesh_resource = "package://two_drones_pkg/mesh/quadrotor.dae";
-    av1.action = visualization_msgs::Marker::ADD;
+    av1.action = visualization_msgs::msg::Marker::ADD;
     av1.pose.orientation.w = 1.0;
     av1.scale.x = av1.scale.y = av1.scale.z = av1.color.a = 1.0;
-    av1.color.r = 0.25;
-    av1.color.g = 0.52;
-    av1.color.b = 1.0;
-    av1.lifetime = ros::Duration(1.0);
+    av1.color.r = .25;
+    av1.color.g = .52;
+    av1.color.b = 1;
+    av1.lifetime = rclcpp::Duration::from_seconds(1.0);
     av2 = av1;
     av2.header.frame_id = "av2";
     av2.ns = "AVs";
     av2.id = 1;
-    av2.color.r = 0.8;
-    av2.color.g = 0.4;
-    av2.color.b = 0.26;
+    av2.color.r = .8;
+    av2.color.g = .4;
+    av2.color.b = .26;
 
     // Trails
     visuals.markers.push_back(av1trail.getMarker());
     visuals.markers.push_back(av2trail.getMarker());
     visuals.markers.push_back(av2trail_rel.getMarker());
-    markers_pub.publish(visuals);
+    markers_pub->publish(visuals);
   }
 };
 
 int main(int argc, char** argv) {
-  ros::init(argc, argv, "plots_publisher_node");
-  PlotsPublisherNode node;
-  ros::spin();
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<PlotsPublisherNode>());
   return 0;
 }
